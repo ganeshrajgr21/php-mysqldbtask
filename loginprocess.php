56c83330ede1fbe0585e44ca8d7ab740c85592e2
Ravivarman J <jravivarman16@gmail.com>
2023-12-28 19:38:06 +0530

    Update loginprocess.php

--- loginprocess.php
+++ loginprocess.php
@@ -19,8 +19,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Retrieve user from the database
-    $sql = "SELECT * FROM users WHERE username='$username'";
+    // Retrieve user from the "data" table
+    $sql = "SELECT * FROM data WHERE username='$username'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -42,4 +42,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 $conn->close();
 ?>
-
